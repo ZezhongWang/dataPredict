e083fcb8bdeace3f15a4433179a949e078587c06
w2w <904192625@qq.com>
2018-01-25 11:53:13 +0800

    Remove djStart file

--- djStart/apps/expert/apps.py
+++
@@ -1,7 +0,0 @@
-# _*_ coding:utf-8 _*_
-from django.apps import AppConfig
-
-
-class ExpertConfig(AppConfig):
-    name = 'apps.expert'
-    verbose_name = "专家信息"
--- djStart/apps/message/apps.py
+++
@@ -1,7 +0,0 @@
-# _*_ coding:utf-8 _*_
-from django.apps import AppConfig
-
-
-class MessageConfig(AppConfig):
-    name = 'apps.message'
-    verbose_name = "用户信息"
